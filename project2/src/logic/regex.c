7f7bbfd144f3edb27637973902d7197b88e1a126
silviacn <silviluc.n@hotmail.com>
2023-10-30 21:44:35 -0600

    WIP initial matrix

--- project2/src/logic/regex.c
+++ project2/src/logic/regex.c
@@ -3,6 +3,8 @@
 
 #include "regex.h"
 #include "matrix.h"
+#include "common.h"
+#include "controller.h"
 
 void fillEquationsMatrix(char ***equations, int **table1, int rows, int columns)
 {
@@ -10,12 +12,36 @@ void fillEquationsMatrix(char ***equations, int **table1, int rows, int columns)
 	{
 		for (int j = 0; j < columns; j++)
 		{
-            if (table1[i][j] > 0) {
+            if (table1[i][j] >= 0) {
                 int landingState = table1[i][j];
-                //snprintf(equations[landingState][i], 1, "%d", j);
-                strcpy(equations[landingState][i], "aaa");
+                char *alphabet = get_symbols();
+
+                char symbol[2];
+                strncpy(symbol, alphabet+j, 1);
+
+                strcpy(equations[landingState][i], symbol);
+
+                // Print matrix
+
+                printf("Print equations matrix:");
+                printf("fila: %d, columna: %d", i, j);
+                printf("{\n");
+                for(int i = 0; i < rows; i++)
+                {
+                    printf("[%d]{", i);
+                    for(int j = 0; j < rows; j++)
+                    {
+                        //if(strlen(equations[i][j] ) == 0 ) 
+                        if(equations[i][j][0] == '\0')
+                        {
+                            strcpy(equations[i][j], "-");
+                        }
+                        printf("%s,", equations[i][j]);
+                    }
+                    printf("}\n");
+                }
+                printf("}\n");
             }
-            //equations[landingState][i] = j;
 		}
 	}
 }
@@ -27,7 +53,7 @@ void fillStartEquationsMatrix(char ***equations, int rows)
 	{
 		for (int j = 0; j < rows; j++)
 		{
-            strcpy(equations[i][j], "-");
+            strcpy(equations[i][j], "");
 		}
 	}
 }
@@ -38,24 +64,4 @@ void createEquationsMatrix(int **table1, int rows, int columns)
 
     fillStartEquationsMatrix(equations, rows);
     fillEquationsMatrix(equations, table1, rows, columns);
-
-	// Print matrix
-
-	printf("Print equations matrix:");
-   	printf("{\n");
-   	for(int i = 0; i < rows; i++)
-	{
-		printf("[%d]{", i);
-		for(int j = 0; j < rows; j++)
-		{
-            //if(strlen(equations[i][j] ) == 0 ) 
-            if(equations[i][j][0] == '\0')
-            {
-                strcpy(equations[i][j], "-");
-            }
-			printf("%s,", equations[i][j]);
-		}
-		printf("}\n");
-	}
-	printf("}\n");
 }
