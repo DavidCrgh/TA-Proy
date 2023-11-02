d3f7064ed970987197383da98cb0f3e2768aaf4e
silviacn <silviluc.n@hotmail.com>
2023-11-01 20:10:25 -0600

    WIP regex algorithm

--- project2/src/logic/regex.h
+++ project2/src/logic/regex.h
@@ -1,6 +1,6 @@
 #ifndef REGEX_H
 #define REGEX_H
 
-void createEquationsMatrix(int **table1, int rows, int columns);
+void createEquationsMatrix(int **table1, int *accept, int rows, int columns);
 
 #endif
